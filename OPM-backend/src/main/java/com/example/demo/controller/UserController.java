5e4e46bee9f3c8fa4ffc0fa5f31ae098ff29e1af
CHANDRUM <74545047+CHANDRUM20@users.noreply.github.com>
2021-04-05 18:42:33 +0530

    Add files via upload

---
+++ OPM-backend/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("api/")
+public class UserController {
+	
+	   @Autowired
+	    private UserRepository userRepository;
+	   
+	   @PostMapping("users")
+	   public User createUser(@RequestBody User user) {
+		   return userRepository.save(user);
+	   }
+
+	    @GetMapping("users")
+	    public List < User > getAllUsers() {
+	        return userRepository.findAll();
+	    }
+
+}
